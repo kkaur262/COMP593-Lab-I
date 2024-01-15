226b5edd139364e0cd6af5e9c3df5c8d39e5d0e8
kkaur262 <kuldeep.kaur4@flemingcollege.ca>
2024-01-15 13:43:31 -0500

    Revert "modified both script file"
    This reverts commit 2ae8da68445ee58010d30c3994ec255a138393c2.

--- script1.py
+++ script1.py
@@ -1,2 +1,2 @@
-# print lines from zen of python
 print("Beautiful is better than ugly")
+print("Simple is better than complex")
--- script2.py
+++
@@ -1,2 +0,0 @@
-print("Complex is better than complicated")
-print("Flat is better than nested")
